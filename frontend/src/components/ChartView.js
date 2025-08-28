ffadc7bf76995a3332d0824b7ef8a829d0037975
Sreekar_Valiveti <valivetisreekar@gmail.com>
2025-08-28 20:36:51 +0530

    Initial commit

--- frontend/src/components/ChartView.js
+++ frontend/src/components/ChartView.js
@@ -11,7 +11,9 @@ import {
   ResponsiveContainer
 } from "recharts";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_BASE || "/api";
+
+
 
 export default function ChartView() {
   const [data, setData] = useState([]);
